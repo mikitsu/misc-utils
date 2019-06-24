da8ccd83f0313e856d5b3ebe7890d4396c4758c3
mik2k2 <44849223+mik2k2@users.noreply.github.com>
2019-06-24 17:15:58 +0200

    support layers of wrapped widgets
    also: 
    - changes to ScrollableWidget
    - minor changes to ValidatedWidget
    - changes to LabeledWidget in line with the move to support for multilayered WrappedWidgets allowing easy access to individual Labels and easy position definition

--- tkstuff.py
+++ tkstuff.py
@@ -13,23 +13,6 @@ GEOMETRY_MANAGERS = ('grid', 'pack', 'place')
 GEOMETRY_MANAGERS_FORGET = [(n, n+'_forget') for n in GEOMETRY_MANAGERS]
 
 
-def modify_call(clsnames_):
-    class OnlyNewOnCall(type):
-        clsnames = clsnames_
-        def __call__(self, *args, **kwargs):
-            if any(self.__name__.endswith(n) for n in self.clsnames):
-                return self.__new__(self, *args, **kwargs)
-            else:
-                r = super().__call__(*args, **kwargs)
-                return r
-    return OnlyNewOnCall
-
-
-def add_new_override(cls):
-    if type(cls).__name__ == 'OnlyNewOnCall':
-        type(cls).clsnames.append(cls.__name__)
-
-
 class ContainingWidget(tk.Widget):
     """Provide a widget that includes other widgets.
 
@@ -44,7 +27,7 @@ class ContainingWidget(tk.Widget):
 
         `widgets` are (<class>, <kwargs>) of the contined widgets
             if positional arguemnts are needed, they may be included
-            under the key '**args**' (clash-safe)
+            under the key '*args' (clash-safe)
         `direction` is a tuple of two elements, respectively one of
             (TOP, BOTTOM) or (RIGHT, LEFT) or the inverse
             and indicates in which direction the contained widgets
@@ -65,29 +48,32 @@ class ContainingWidget(tk.Widget):
         self.base = base(master)
         self.base.container_widget = self
         self.widgets = tuple(w[0](self.base,
-                                  *w[1].pop('**args**', ()),
+                                  *w[1].pop('*args', ()),
                                   **w[1]) for w in widgets)
         self.direction = direction
         self.horizontal = horizontal
         self.vertical = vertical
 
     def __getattr__(self, name):
+        if name == 'container_widget':
+            raise AttributeError('{!r} has no attribute "container_widget"'.format(self))
         return getattr(self.base, name)
 
     def _geo_wrapper(name, forget):
-        def wrapper(self, *args, **kwargs):
+        def wrapper(self, *args, rcoords=None, **kwargs):
             getattr(self.base, name)(*args, **kwargs)
-            self._grid_subwidgets()
+            return self.grid_subwidgets(rcoords)
         wrapper.__name__ = name
         wrapper.__doc__ = """.{}() the base widget and .grid() the subwidgets.
 
                             respect the directions given in __init__
                             pass *args and **kwargs to the base widget
                             """.format(name)
-        def forgetter(self):
+        def forgetter(self, exclude=None):
             getattr(self.base, forget)()
             for widget in self.widgets:
-                widget.grid_forget()
+                if widget is not exclude:
+                    widget.grid_forget()
         forgetter.__name__ = forget
         forgetter.__doc__ = ".{}() the base widget and .grid_forget() the subwidgets".format(forget)
         return wrapper, forgetter
@@ -96,9 +82,15 @@ class ContainingWidget(tk.Widget):
         locals()[name], locals()[forget] = _geo_wrapper(name, forget)
     del _geo_wrapper
 
-    def _grid_subwidgets(self):
-        """.grid() the subwidgets according to `self.direction`, `self.horizontal` and `self.vertical`"""
-        # x and y _S_tart and _I_ncrement
+    def grid_subwidgets(self, rcoords):
+        """.grid() the subwidgets according to `self.direction`,
+                `self.horizontal` and `self.vertical`
+
+            `rcoords` specifies a widget (identity comparison) that
+                will not have .grid() called upon it. Instead, the x and y
+                coordinates of its position on the grid will be returned"""
+        # x and y Start,  Increment and Return
+        xr, yr = -1, -1
         if tk.RIGHT in self.direction:
             xs = 0
             xi = 1
@@ -123,10 +115,11 @@ class ContainingWidget(tk.Widget):
             raise ValueError('`direction` must be of the form specified in __init__')
         x, y = xs, ys
         for widget in self.widgets:
-            # multi-depth WrappedWidget. Find the actual container
-            while widget not in self.base.children.values():
-                widget = widget.master
-            widget.grid(row=y, column=x)
+            print('cw', '.grid', repr(widget), x, y)
+            if widget is rcoords:
+                xr, yr = x, y
+            else:
+                widget.grid(row=y, column=x)
             if self.direction[0] in (tk.RIGHT, tk.LEFT):
                 x += xi
             elif self.direction[0] in (tk.TOP, tk.BOTTOM):
@@ -137,9 +130,10 @@ class ContainingWidget(tk.Widget):
             if x in (-1, self.horizontal or None):
                 x = xs
                 y += yi
+        return xr, yr
 
 
-class ProxyWidget(tk.Widget):
+class BaseProxyWidget(tk.Widget):
     """Provide a widget that delegates some lookups to a .container
         in a way compatile with ContainningWidget
 
@@ -157,40 +151,46 @@ class ProxyWidget(tk.Widget):
         """Create a new ProxyWidget. `container` is the container,
             other arguments are passed along"""
         super().__init__(*args, **kwargs)
+        self.proxy_init(container)
+
+    def proxy_init(self, container):
+        self.__dict__.setdefault('container_list', [])
         if container is not None:
             self.container = container
+            self.container_list.append(container)
 
     def _geo_wrapper(name, forget):
         def wrapper(self, *args, **kwargs):
-            if self.__visible == 1:
-                self.__visible = 2
-                getattr(super(), name)(*args, **kwargs)
-            elif self.__visible == 0:
-                self.__visible = 1
-                getattr(self.container, name)(*args, **kwargs)
+            self.__visible = True
+            container_list = reversed(self.container_list)
+            x, y = getattr(next(container_list), name
+                           )(*args, rcoords=self, **kwargs)
+            for container in container_list:  # is an iterator
+                x, y = container.grid(row=y, column=x, rcoords=self)
+            tk.Widget.grid(self, row=y, column=x)
         def forgetter(self):
-            if self.__visible == 2:
-                self.__visible = 1
-                getattr(self.container, forget)()
-            elif self.__visible == 1:
-                self.__visible = 0
-                getattr(super(), forget)()
+            container_list = reversed(self.container_list)
+            getattr(next(container_list), forget)(self)
+            for container in container_list:
+                container.grid_forget(self)
+            tk.Widget.grid_forget(self)
         wrapper.__name__ = name
         forgetter.__name__ = forget
         return wrapper, forgetter
 
-    __visible = False
     for name, forget in GEOMETRY_MANAGERS_FORGET:
         locals()[name], locals()[forget] = _geo_wrapper(name, forget)
     del _geo_wrapper
 
 
-class BaseWrappedWidget(ProxyWidget, metaclass=modify_call(
-        ['BaseWrappedWidget', 'WrappedWidget', 'LabeledWidget'])):
+class ProxyWidget(BaseProxyWidget):
+    pass
+
+
+class BaseWrappedWidget(BaseProxyWidget):
     """Provide a widget that is contained inside a
         ContainingWidget along others but provides normal access"""
 
-    #@classmethod  # __new__ is complicated because the class is istantiated later
     def __new__(cls, master, main_widget, *auxiliary_widgets, container_kw={}):
         """Create a new WrappedWidget.
 
@@ -201,17 +201,29 @@ class BaseWrappedWidget(ProxyWidget, metaclass=modify_call(
         `main_widget` and each of the `auxiliary_widgets`
             are (<class>, <kwargs>)"""
         main_cls, main_kw = main_widget
+        if main_cls in cls.mro():
+            # multiple wrapping
+            bases = (main_cls,)
+        elif cls in main_cls.mro():
+            bases = (cls,)
+        else:
+            bases = (cls, main_cls)
+        if main_cls.__new__ is object.__new__:
+            def __new__(cls, *si, **nk):
+                print('sunk')
+                return object.__new__(cls)
+        else:
+            __new__  = main_cls.__new__
         main_cls = type('Wrapped'+main_cls.__name__,
-                        (main_cls, cls),
-                        {'__new__': main_cls.__new__,
-                         '__init__': main_cls.__init__
-                         })
+                        bases,
+                        {'__new__': __new__,})
         container = ContainingWidget(master,
                                      (main_cls, main_kw),
                                      *auxiliary_widgets,
                                      **container_kw)
         self = container.widgets[0]
-        self.container=container
+        self.proxy_init(container)
+        type(self).__init__ = lambda *si, **nk: None
         return self
 
 
@@ -220,16 +232,32 @@ class WrappedWidget(BaseWrappedWidget):
 
 
 class LabeledWidget(BaseWrappedWidget):
-    """Convenience class for widgets to be displayed with a Label"""
-    def __new__(cls, master, widget, text, **options):
+    """Convenience class for widgets to be displayed with a Label
+
+        Provide a .labels dict to provide direct access to all labels
+        New labels may be added with the add_label method"""
+    def __new__(cls, master, widget, text,
+                position=tk.LEFT,
+                label_id='label',
+                **options):
         """Create a new WrappedWidget with a Label with the selected text.
 
+            `position` describes the position of the Label relative to the
+                wrapped widget and may be one of {tk.TOP, tk.BOTTOM, tk.LEFT, tk.RIGHT}
+                if the option 'direction' is present, it overrides the automatic
+                direction chosen for the position.
             `options` are passed
-            by default, the Label is shown to the left, this may be overridden in `options`"""
-        kw = {'direction': (tk.LEFT, tk.BOTTOM)}
+            """
+        kw = {'direction': (position, (tk.TOP if position in (tk.LEFT, tk.RIGHT) else tk.BOTTOM))}
         kw.update(options)
-        return super().__new__(cls, master, widget, (tk.Label, {'text': text}),
-                           container_kw=kw)
+        self = super().__new__(cls, master, widget, (tk.Label, {'text': text}),
+                               container_kw=kw)
+        labels = {label_id: self.container.widgets[1]}
+        try:
+            self.labels.update(labels)  # we already have a LabeledWidget somewhere
+        except AttributeError:
+            self.labels = labels
+        return self
 
 
 class ScrollableWidget(tk.Widget):
@@ -247,6 +275,10 @@ class ScrollableWidget(tk.Widget):
 
             >>> ScrollableLabel = ScrollableWidget(...)(tk.Label)
 
+        Note: Due to technical reasons, when making a Widget that suppresses
+            the call of __init__, init_here=True must be
+            passed to __new__ to ensure proper functioning
+
         +-------------------------------------------------+
         | WARNING: multiple calling of a geometry manager |     
         |          *will*  *mess*  *things*  *up*         |
@@ -258,7 +290,7 @@ class ScrollableWidget(tk.Widget):
 
     def __call__(self, wrapped_cls):
         class NewClass(ProxyWidget, wrapped_cls):
-            def __new__(cls, master, cnf={}, **kw):
+            def __new__(cls, master, *a, init_here=False, **kw):
                 container = ContainingWidget(master,  # attention: order matters and is used
                                              (tk.Canvas, {'width': self.width,
                                                           'height': self.height}),
@@ -267,13 +299,20 @@ class ScrollableWidget(tk.Widget):
                 canvas, scrollbar = container.widgets
                 canvas.config(**{self.direction+'scrollcommand': scrollbar.set})
                 scrollbar.config(command=getattr(canvas, self.direction+'view'))
-                inst = wrapped_cls.__new__(cls)
+                if wrapped_cls.__new__ is object.__new__:
+                    inst = object.__new__(cls)
+                else:
+                    inst = wrapped_cls.__new__(cls, canvas, *a, **kw)
                 inst.container = container  # other init done afterwards
+                inst.direction = self.direction
+                if init_here:
+                    inst.container.widgets[0].create_window((0, 0), window=inst.master)
                 return inst
 
-            def __init__(self, master=None, cnf={}, **kw):
-                super().__init__(master, cnf, **kw)
-                self.container.widgets[0].create_window((0, 0), window=self)
+            def __init__(self, master, *args, **kwargs):
+                canvas = self.container.widgets[0]
+                super().__init__(canvas, *args, **kwargs)
+                canvas.create_window((0, 0), window=self)
                 
 
             def set_scrollregion(self):
@@ -281,18 +320,16 @@ class ScrollableWidget(tk.Widget):
                 sw, sh = self.winfo_width(), self.winfo_height()
                 canvas.config(scrollregion=(-sw//2, -sh//2, sw//2, sh//2))
 
-            def _wait_for_scrollregion(self, t, initial, ct=10):
-                if (self.winfo_width(), self.winfo_height()) == (1, 1) and ct:
-                    self.after(t, self._wait_for_scrollregion, t, initial, ct-1)
-                else:
-                    self.set_scrollregion()
-
             def _geo_wrapper(name, forget):
                 def wrapper(self, *args, **kwargs):
-                    initial = self.winfo_width(), self.winfo_height()
                     getattr(super(), name)(*args, **kwargs)
-                    self.container.widgets[1].grid(row=0, column=1, sticky=tk.NS)
-                    self.after(0, self._wait_for_scrollregion, 1, initial)
+                    if isinstance(self, ContainingWidget):
+                        self._grid_subwidgets()
+                    if isinstance(self, BaseWrappedWidget):
+                        x, y = self.master.container_widget.grid_subwidgets(self)
+                        tk.Grid.grid(self, row=y, column=x)
+                    sticky = {'y': tk.NS, 'x': tk.EW}[self.direction]
+                    self.container.widgets[1].grid(row=0, column=1, sticky=sticky)
                 wrapper.__name__ = name
                 def wrapper_forget(self):
                     getattr(self.container, forget)()
@@ -304,6 +341,8 @@ class ScrollableWidget(tk.Widget):
             del _geo_wrapper
 
         NewClass.__name__ = 'Scrollable'+wrapped_cls.__name__
+        NewClass.__qualname__ = '.'.join(NewClass.__qualname__.rsplit('.', 1)[:-1]
+                                         +[NewClass.__name__])
         return NewClass
         
 
@@ -314,7 +353,7 @@ class ValidatedWidget(tk.Widget):
     def new_cls(cls, widget, validator, getter=None):
         """Create a new widget class
 
-            create a dynamic subclass of VilidatedWidget and the passed `widget`
+            create a dynamic subclass of ValidatedWidget and the passed `widget`
             `validator` should take the widget's input
                 and may also be set on instances separately
             `getter` provides the name of the function to use for getting
@@ -356,7 +395,7 @@ class ValidatedWidget(tk.Widget):
                 with the gven arguements"""
         return cls.new_cls(widget, validator, getter)(master, **widgetkw)
     
-    def get(self):
+    def validate(self):
         return self.validator(self.getter())
 
 
@@ -379,4 +418,5 @@ class RadioChoiceWidget(ContainingWidget):  # yay, no class creation magic, just
 
     def get(self):
         return self.var.get()
+        
 
