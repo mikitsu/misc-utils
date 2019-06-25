67540e9b302d823c1e94d5396252224878307b6d
mik2k2 <44849223+mik2k2@users.noreply.github.com>
2019-06-25 16:15:52 +0200

    Add FormDialog and WidgetDialog
    FormDialog allows creating a dialog with a form
    WidgetDialog puts a single widget into the dialog, e.g. a RadioChoiceWIdget

--- tkstuff/dialogs.py
+++ tkstuff/dialogs.py
@@ -1,1 +1,52 @@
 """An extension to tkinter.simpledialog"""
+
+import tkinter as tk
+import tkinter.simpledialog as tk_dia
+import misc.tkstuff as mtk
+import misc.tkstuff.forms as mtkf
+
+class UserExitiedDialog(Exception):
+    pass
+
+
+class FormDialog(tk_dia.Dialog):
+    """A dialog for forms"""
+    def __init__(self, parent, form, title=None):
+        self.form_onsubmit = form._Form__formwidget_options.get('onsubmit')
+        form._Form__formwidget_options['onsubmit'] = self.ok
+        self.form = form
+        super().__init__(parent, title=title)
+
+    def body(self, master):
+        self.form_widget = self.form(master)
+        self.form_widget.pack()
+
+    def buttonbox(self):
+        pass
+
+    def apply(self):
+        if self.form_onsubmit is not None:
+            self.form_onsubmit(self.form_widget.data)
+        self.result = self.form_widget.data
+
+
+class WidgetDialog(tk_dia.Dialog):
+    """A dialog for getting input from any widget"""
+    def __init__(self, parent, widget, widget_kw={}, title=None, getter=None):
+        self.widget_cls = widget
+        self.widget_cnf = widget_cnf
+        self.getter = mtk.get_getter(widget, getter)
+        super().__init__(parent, title)
+
+    def body(self, master):
+        self.widget = self.widget_cls(master, **self.widget_kw)
+        self.widget.pack()
+
+    def validate(self):
+        try:
+            return self.widget.validate()[0]
+        except AttributeError:
+            return True
+
+    def apply(self):
+        self.result = self.getter(self.widget)
