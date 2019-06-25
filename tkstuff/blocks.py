812e4d17861d03d3fa206d9a05c6d371afe9d194
mik2k2 <44849223+mik2k2@users.noreply.github.com>
2019-06-25 15:26:25 +0200

    Update blocks.py
    Add:
    - PasswordEntry
    - IntEntry (ValidatedEntry)
    - LoginForm

--- tkstuff/blocks.py
+++ tkstuff/blocks.py
@@ -1,1 +1,17 @@
 """Some prepared widgets, forms and functions"""
+
+import tkinter as tk
+import misc.tk as mtk
+import misc.tk.forms as mtkf
+
+class PasswordEntry(tk.Entry):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, show='*', **kwargs)
+
+
+IntEntry = mtk.ValidatedWidget.new_cls(tk.Entry, misc.Validator(int))
+
+
+class LoginForm(mtkf.Form):
+    username: mtkf.Element = tk.Entry
+    password: mtkf.Element = PasswordEntry
